687098347445568a6b30d3d6e433ddd9d378e7c8
MaudorElConquistador <mauricioruiz01@live.com>
2020-01-09 19:38:24 -0600

    first commit

---
+++ cap.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/*
+    array number i{1,2,3,5,3}
+    array number p{1,2,3,5}
+*/
+
+void banner(){
+    cout << R"(
+     __  __                 _            ______ _  _____                        _     _            _            
+    |  \/  |               | |          |  ____| |/ ____|                      (_)   | |          | |           
+    | \  / | __ _ _   _  __| | ___  _ __| |__  | | |     ___  _ __   __ _ _   _ _ ___| |_ __ _  __| | ___  _ __ 
+    | |\/| |/ _` | | | |/ _` |/ _ \| '__|  __| | | |    / _ \| '_ \ / _` | | | | / __| __/ _` |/ _` |/ _ \| '__|
+    | |  | | (_| | |_| | (_| | (_) | |  | |____| | |___| (_) | | | | (_| | |_| | \__ \ || (_| | (_| | (_) | |   
+    |_|  |_|\__,_|\__,_|\__,_|\___/|_|  |______|_|\_____\___/|_| |_|\__, |\__,_|_|___/\__\__,_|\__,_|\___/|_|   
+                                                                        | |                                      
+                                                                        |_|                                      
+<<=============================================================================================================>
+    )"<< endl;
+}
+
+bool is_capi(string capicua){
+    int n;
+    int longitud = capicua.size() - 1;
+    if (longitud%2 != 0){
+        n = (longitud +1)/2;
+    }
+    n = longitud/2;
+    for (int i = 0; i <= n; i++){
+        if (capicua[i] != capicua[longitud-i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int gen_capi(string cap){
+    int capicua = 1;
+    string capr = cap;
+    if (is_capi(cap)){
+        return stoi(cap);
+    }
+    reverse(capr.begin(), capr.end());
+    cout << "Numero suma " << to_string(stoi(cap) + stoi(capr)) << endl;
+    /*capicua = stoi(cap) + stoi(capr);
+    gen_capi(to_string(capicua));*/
+    return gen_capi(to_string(stoi(cap) + stoi(capr)));
+}
+
+int main(){
+    banner(); 
+    string capicua;
+    cout << "Ingresa el numero del cual quieras generar un capicua" << endl;
+    cin >> capicua;
+    int resultado = gen_capi(capicua);
+    cout << "El resultado es " << resultado << endl ;
+    return 0;
+}
